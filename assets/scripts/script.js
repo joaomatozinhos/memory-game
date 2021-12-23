31f439cf5e156fb640430e0e4c84e82b497d3c6f
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2021-12-23 11:32:01 -0300

    add function show flip cards

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,5 @@
 let board = document.getElementById('board')
-
-const front = 'card_front'
-const back = 'card_back'
+let cardList = document.getElementsByClassName('cards')
 
 let techs = [
   'html',
@@ -74,4 +72,24 @@ function shuffleCards(cards) {
       cards[randomIndex]
     ]
   }
+}
+
+// função para exibir cartas na tela
+function showCards(cards) {
+  for (let card of cards) {
+    board.innerHTML += `
+      <div id="${card.id}" class="card" data-icon="${card.icon}" onclick="flipCard(${card.id})">
+        <div class="card-front">
+          <img src="assets/images/${card.icon}.png"/>
+        </div>
+        <div class="card-back">&lt/&gt</div>
+      </div>
+    `
+  }
+}
+
+// função para virar a carta
+function flipCard(card) {
+  console.log(card)
+  card.classList.add('flip')
 }
